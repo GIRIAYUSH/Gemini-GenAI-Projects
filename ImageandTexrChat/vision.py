24dd8477ab05a25c41a4532908cf7f249de4e8ee
GIRI_AYUSH <giri.ayush2024@gmail.com>
2024-06-20 23:12:16 +0530

    second commit

---
+++ ImageandTexrChat/vision.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+from PIL import Image
+
+genai.configure(api_key=os.getenv("Gemini_Api"))
+
+model=genai.GenerativeModel("gemini-pro-vision")
+
+def get_gemini_response(input,image):
+    if input!="":
+
+        response=model.generate_content([input,image])
+    else:
+        respnse=model.generate_content(image)
+
+    return response.text
+
+st.set_page_config(page_title="Gemini Vision Application")
+st.header("Gemini Application")
+
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file=st.file_uploader("Choose an image...",type=["jpg","jpeg","png","svg"])
+image=""
+if uploaded_file is not None:
+    image=Image.open(uploaded_file)
+    st.image(image,caption="Uploaded Image.",use_column_width=True)
+
+submit=st.button("Response")
+if submit:
+    response=get_gemini_response(input,image)
+    st.subheader("the reponse is")
+    st.write(response)
+
+
+
+
+
+    
+
